c17c4e8d704532ea5f59cbfc6a490d3ce1d4548c
Adrian Polo <adrian.polo.romani@gmail.com>
2025-12-15 14:30:30 -0500

    Added css to login

--- src/app/components/login.component.ts
+++ src/app/components/login.component.ts
@@ -7,7 +7,8 @@ import { Router } from '@angular/router'
 	selector: 'app-login',
 	standalone: true,
 	imports: [CommonModule, FormsModule],
-	templateUrl: './login.component.html'
+	templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
 	username = ''
