1ac60ab085813bcee2364be171436239bb6f7669
Adrian Polo <adrian.polo.romani@gmail.com>
2025-12-15 18:28:57 -0500

    Updated app.config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './components/login.component';
+import { Home } from './components/home/home';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
+    { path: '', component: Home }, 
 	{ path: 'login', component: LoginComponent },
-	//{ path: '', redirectTo: 'login', pathMatch: 'full' }
+    { path: '**', redirectTo: '' }
 ];
