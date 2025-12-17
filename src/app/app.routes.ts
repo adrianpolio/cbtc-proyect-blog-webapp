e700e222026c3b018dc314105589490086c42d7f
Adrian Polo <adrian.polo.romani@gmail.com>
2025-12-16 21:56:11 -0500

    Temp home is blogs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { BlogDetailComponent } from './components/blog-detail/blog-detail';
 import { BlogListComponent } from './components/blog-list/blog-list';
 
 export const routes: Routes = [
-    { path: '', component: Home, pathMatch: 'full' }, 
+    { path: '', component: BlogListComponent, pathMatch: 'full' }, 
 	{ path: 'login', component: LoginComponent },
     { path: 'blogs/:id', component: BlogDetailComponent },
     { path: 'blogs', component: BlogListComponent },    
