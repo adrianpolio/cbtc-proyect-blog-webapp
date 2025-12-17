810e1a91983ab4f8ce46433c6ef5fd65fe0f8a6c
Mariella Castro <i202411929@cibertec.edu.pe>
2025-12-16 20:47:15 -0500

    sending route correction

--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Blog } from '../models/blog.model';
-import { environment } from '.././environments/environment';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
